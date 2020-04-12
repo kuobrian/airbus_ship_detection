0421a044fb9bf3b806dee16c23734db4cdde9028
unknown <kuobrian614@gmail.com>
2020-04-12 23:12:51 +0800

    train classifier resnet_ship

--- unet/unet_with_resnet34.py
+++ unet/unet_with_resnet34.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
+import torch.nn as nn
 from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
@@ -65,70 +66,10 @@ class AirbusDataset(Dataset):
         if self.transform is not None :
             imgO = self.transform(imgO)
         
-        return imgO,  torch.LongTensor([label])
+        return imgO,  label
     
     def __len__(self):
         return self.train_sample.shape[0]
-
-def train_model(model, critertion, optimizer, scheduler, num_epochs=25, mode="train"):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        if mode == "train":
-            model.train()
-        else:
-            model.eval()
-            
-        running_loss = 0.0
-        running_corrects = 0
-
-        for inputs, labels in dataloaders[phase]:
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            
-            optimizer.zero_grad()
-
-            with torch.set_grad_enabled(phase == "train"):
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
-
-                if phase =="train":
-                    loss.backward()
-                    optimizer.step()
-            running_loss += loss.item() * inputs.size(0)
-            running_corrects += torch.sum(preds == labels.data)
-            
-        #     if phase == "train":
-        #         scheduler.step()
-
-        #     epoch_loss = running_loss / dataset_sizes[phase]
-        #     epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-        #     print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-        #         phase, epoch_loss, epoch_acc))
-
-        #     # deep copy the model
-        #     if phase == 'val' and epoch_acc > best_acc:
-        #         best_acc = epoch_acc
-        #         best_model_wts = copy.deepcopy(model.state_dict())
-
-        # print()
-
-    # time_elapsed = time.time() - since
-    # print('Training complete in {:.0f}m {:.0f}s'.format(
-    #     time_elapsed // 60, time_elapsed % 60))
-    # print('Best val Acc: {:4f}'.format(best_acc))
-
-    # # load best model weights
-    # model.load_state_dict(best_model_wts)
-    return model
 
 def pre_process_data(data_dir):
     train = pd.read_csv(os.path.join(data_dir, "train_ship_segmentations_v2.csv"))
@@ -160,7 +101,34 @@ def pre_process_data(data_dir):
     train_path = os.path.join(data_dir, "train_v2")
     test_path = os.path.join(data_dir, "test_v2")
 
+
+
+class CustomResnet34(nn.Module):
+    def __init__(self, num_classes, training = True):
+        super().__init__()
+        self.training = training
+        self.num_classes = num_classes
+        self.resnet = models.resnet34(pretrained=True)
+        # for param in self.resnet.parameters():
+        #     param.requires_grad = False
+        self.in_num_ftrs = self.resnet.fc.in_features
+        self.out_num_ftrs = self.resnet.fc.out_features
+
+        self.fc1 = nn.Linear(self.out_num_ftrs, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        x = nn.ReLU()(self.fc1(x))
+        x = nn.functional.dropout(x, p=0.5, training=self.training)
+        x = nn.ReLU()(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 if __name__ == "__main__":
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     csv_file = "train_ship_segmentations_v2.csv"
     data_dir = "./data_airbus"
     
@@ -176,36 +144,47 @@ if __name__ == "__main__":
     # imgO, img, lbl = airbus_dataset[2]
 
     train_loader = DataLoader(dataset=airbus_dataset,
-                            batch_size=2,
+                            batch_size=32,
                             shuffle=False,
                             num_workers=1)
     y_onehot = torch.FloatTensor(2, 2)
 
-    for imgO, label in train_loader:
-    
-        y_onehot.zero_()
-        y_onehot.scatter_(1, label, 1)
-        # assert(0)
-    
-    
-    
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    
-    resnet34 = models.resnet34(pretrained=True)
-    for param in resnet34.parameters():
-        param.requires_grad = True
+    # resnet34 = models.resnet34(pretrained=True)
 
-    in_num_ftrs = resnet34.fc.in_features
-    out_num_ftrs = resnet34.fc.out_features
-    resnet34.fc = nn.Linear(in_num_ftrs, 2)
 
-    
+    resnet34 = CustomResnet34(2)
+    # for param in resnet34.parameters():
+    #     print(param.requires_grad)
+
     resnet34 = resnet34.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer_resnet = optim.SGD(resnet34.fc.parameters(), lr=0.001, momentum=0.9)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+    optimizer_resnet = optim.SGD(resnet34.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_resnet, step_size=7, gamma=0.1)
 
-    model_resnet = train_model(resnet34, criterion, optimizer_resnet, exp_lr_scheduler, num_epochs=25)
+
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, (imgO, labels) in enumerate(train_loader, 0):
+
+            # y_onehot.zero_()
+            # y_onehot.scatter_(1, labels, 1)
+            # print(imgO.shape)
+            # print(y_onehot.shape)
+            # pass 
+
+            imgO = imgO.to(device)
+            labels = labels.to(device)
+            optimizer_resnet.zero_grad()
+
+            outputs = resnet34(imgO)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer_resnet.step()
+
+
+        print('[%d] loss: %.3f' %  (epoch + 1, running_loss))
+    
 
     
