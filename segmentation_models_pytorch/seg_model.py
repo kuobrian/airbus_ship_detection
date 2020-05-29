c03c1cce96480ef31f8830c7b41dad2dd84c6137
Brian Guo <kuobrian614@gmail.com>
2020-05-29 14:51:09 +0800

    train the Uresnet34 model

--- segmentation_models_pytorch/seg_model.py
+++ segmentation_models_pytorch/seg_model.py
@@ -204,143 +204,146 @@ Visualize resulted augmented images and masks
 import torch
 import numpy as np
 import segmentation_models_pytorch as smp
-os.environ['CUDA_VISIBLE_DEVICES'] = "3, 4, 5"
+# os.environ['CUDA_VISIBLE_DEVICES'] = "3, 4, 5"
 ENCODER = "se_resnext50_32x4d"
 ENCODER_WEIGHTS = "imagenet"
 CLASSES = ["car"]
 ACTIVATION = "sigmoid" # could be None for logits or 'softmax2d' for multicalss segmentation
 DEVICE = "cuda"
 
-
-
-
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
 ''' Train Step '''
 
-# # create segmentation model with pretrained encoder
-# model = smp.FPN(
-#     encoder_name=ENCODER, 
-#     encoder_weights=ENCODER_WEIGHTS, 
-#     classes=len(CLASSES), 
-#     activation=ACTIVATION,
-# )
-preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+
+train_dataset = Dataset(
+    x_train_dir, 
+    y_train_dir, 
+    augmentation=get_training_augmentation(), 
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+valid_dataset = Dataset(
+    x_valid_dir, 
+    y_valid_dir, 
+    augmentation=get_validation_augmentation(), 
+    preprocessing=get_preprocessing(preprocessing_fn),
+    classes=CLASSES,
+)
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
+valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
 
 
 
 
-# train_dataset = Dataset(
-#     x_train_dir, 
-#     y_train_dir, 
-#     augmentation=get_training_augmentation(), 
-#     preprocessing=get_preprocessing(preprocessing_fn),
-#     classes=CLASSES,
-# )
 
-# valid_dataset = Dataset(
-#     x_valid_dir, 
-#     y_valid_dir, 
-#     augmentation=get_validation_augmentation(), 
-#     preprocessing=get_preprocessing(preprocessing_fn),
-#     classes=CLASSES,
-# )
+# create segmentation model with pretrained encoder
+model = smp.FPN(
+    encoder_name=ENCODER, 
+    encoder_weights=ENCODER_WEIGHTS, 
+    classes=len(CLASSES), 
+    activation=ACTIVATION,
+)
 
-# train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
-# valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+
+
 
 loss = smp.utils.losses.DiceLoss()
 metrics = [
     smp.utils.metrics.IoU(threshold=0.5),
 ]
 
-# optimizer = torch.optim.Adam([ 
-#     dict(params=model.parameters(), lr=0.0001),
-# ])
+optimizer = torch.optim.Adam([ 
+    dict(params=model.parameters(), lr=0.0001),
+])
 
-# train_epoch = smp.utils.train.TrainEpoch(
-#     model, 
-#     loss=loss, 
-#     metrics=metrics, 
-#     optimizer=optimizer,
-#     device=DEVICE,
-#     verbose=True,
-# )
-
-# valid_epoch = smp.utils.train.ValidEpoch(
-#     model, 
-#     loss=loss, 
-#     metrics=metrics, 
-#     device=DEVICE,
-#     verbose=True,
-# )
-
-
-# # train model for 40 epochs
-
-# max_score = 0
-
-
-# for i in range(0, 40):
-    
-#     print('\nEpoch: {}'.format(i))
-#     train_logs = train_epoch.run(train_loader)
-#     valid_logs = valid_epoch.run(valid_loader)
-    
-#     # do something (save model, change lr, etc.)
-#     if max_score < valid_logs['iou_score']:
-#         max_score = valid_logs['iou_score']
-#         torch.save(model, './best_model.pth')
-#         print('Model saved!')
-        
-#     if i == 25:
-#         optimizer.param_groups[0]['lr'] = 1e-5
-#         print('Decrease decoder learning rate to 1e-5!')
-
-''' Test Step '''
-best_model = torch.load('./best_model.pth')
-
-# create test dataset
-test_dataset = Dataset(
-    x_test_dir, 
-    y_test_dir, 
-    augmentation=get_validation_augmentation(), 
-    preprocessing=get_preprocessing(preprocessing_fn),
-    classes=CLASSES,
-)
-
-test_dataloader = DataLoader(test_dataset)
-
-# evaluate model on test set
-test_epoch = smp.utils.train.ValidEpoch(
-    model=best_model,
-    loss=loss,
-    metrics=metrics,
+train_epoch = smp.utils.train.TrainEpoch(
+    model, 
+    loss=loss, 
+    metrics=metrics, 
+    optimizer=optimizer,
     device=DEVICE,
+    verbose=False,
 )
 
-logs = test_epoch.run(test_dataloader)
-
-
-# test dataset without transformations for image visualization
-test_dataset_vis = Dataset(
-    x_test_dir, y_test_dir, 
-    classes=CLASSES,
+valid_epoch = smp.utils.train.ValidEpoch(
+    model, 
+    loss=loss, 
+    metrics=metrics, 
+    device=DEVICE,
+    verbose=False,
 )
 
 
-for i in range(5):
-    n = np.random.choice(len(test_dataset))
+# train model for 40 epochs
+
+max_score = 0
+
+
+for i in range(0, 40):
     
-    image_vis = test_dataset_vis[n][0].astype('uint8')
-    image, gt_mask = test_dataset[n]
+    print('\nEpoch: {}'.format(i))
+    train_logs = train_epoch.run(train_loader)
+    valid_logs = valid_epoch.run(valid_loader)
     
-    gt_mask = gt_mask.squeeze()
-    
-    x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
-    pr_mask = best_model.predict(x_tensor)
-    pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+    # do something (save model, change lr, etc.)
+    if max_score < valid_logs['iou_score']:
+        max_score = valid_logs['iou_score']
+        torch.save(model, './best_model.pth')
+        print('Model saved!')
         
-    visualize(
-        image=image_vis, 
-        ground_truth_mask=gt_mask, 
-        predicted_mask=pr_mask
-    )
+    if i == 25:
+        optimizer.param_groups[0]['lr'] = 1e-5
+        print('Decrease decoder learning rate to 1e-5!')
+
+# ''' Test Step '''
+# best_model = torch.load('./best_model.pth')
+
+# # create test dataset
+# test_dataset = Dataset(
+#     x_test_dir, 
+#     y_test_dir, 
+#     augmentation=get_validation_augmentation(), 
+#     preprocessing=get_preprocessing(preprocessing_fn),
+#     classes=CLASSES,
+# )
+
+# test_dataloader = DataLoader(test_dataset)
+
+# # evaluate model on test set
+# test_epoch = smp.utils.train.ValidEpoch(
+#     model=best_model,
+#     loss=loss,
+#     metrics=metrics,
+#     device=DEVICE,
+# )
+
+# logs = test_epoch.run(test_dataloader)
+
+
+# # test dataset without transformations for image visualization
+# test_dataset_vis = Dataset(
+#     x_test_dir, y_test_dir, 
+#     classes=CLASSES,
+# )
+
+
+# for i in range(5):
+#     n = np.random.choice(len(test_dataset))
+    
+#     image_vis = test_dataset_vis[n][0].astype('uint8')
+#     image, gt_mask = test_dataset[n]
+    
+#     gt_mask = gt_mask.squeeze()
+    
+#     x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+#     pr_mask = best_model.predict(x_tensor)
+#     pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+        
+#     visualize(
+#         image=image_vis, 
+#         ground_truth_mask=gt_mask, 
+#         predicted_mask=pr_mask
+#     )
--- unet/image_preprocess.py
+++ unet/image_preprocess.py
@@ -9,6 +9,8 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import torch
 from PIL import Image
+import torch.nn.functional as F
+import torch.nn as nn
 from resnet34Unet import Resnet34Unet
 
 
@@ -30,7 +32,7 @@ def rle_decode(mask, shape=(768, 768)):
     return img.reshape(shape).T
 
 def masks_as_image(in_mask_list):
-    all_masks = np.zeros((768, 768), dtype = np.int16)
+    all_masks = np.zeros((768, 768), dtype=np.uint8)
     for mask in in_mask_list:
         if isinstance(mask, str):
             all_masks += rle_decode(mask)
@@ -95,27 +97,141 @@ def preprocess(data_dir, train_path, test_path):
     return balanced_train_df, valid_df
 
 class AirbusDataset(Dataset):
-    def __init__(self, data_dir, train_set, transform=None):
+    def __init__(self, data_dir, train_set, img_transform=None, mask_transform=None):
         self.data_dir = data_dir
 
         self.train_set = train_set
 
-        self.transform = transform
+        self.img_transform = img_transform
+        self.mask_transform = mask_transform
         
     def  __getitem__(self ,index):
         image_name = self.train_set.iloc[index]["ImageId"]
         imgpath = os.path.join(self.data_dir, "train_v2/"+image_name)
         imgO = Image.open(imgpath).convert('RGB')
-        if self.transform is not None :
-            imgO = self.transform(imgO)
+
+        if self.img_transform is not None :
+            imgO = self.img_transform(imgO)
+
         rle_pixels = []
         rle_pixels.append(self.train_set.iloc[index]["EncodedPixels"])
-        mask = masks_as_image(rle_pixels)
         
-        return imgO,  mask
+        mask = masks_as_image(rle_pixels)
+        if self.mask_transform is not None:
+            mask = self.mask_transform(mask)
+
+        return imgO,  mask.float()
     
     def __len__(self):
         return self.train_set.shape[0]
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x, y):
+        nums = y.size(0)
+        smooth = 1.
+        x = F.sigmoid(x)
+        x_flat = x.view(nums, -1)
+        y_flat = y.view(nums, -1)
+        
+        intersection = x_flat * y_flat
+        loss = 2 * (intersection.sum(1) + smooth) / (x_flat.sum(1) + y_flat.sum(1) + smooth)
+        loss = loss.sum() / nums
+        
+        return loss
+
+class BCEDiceWithLogitsLoss(nn.Module):
+    def __init__(self, dice_weight=1, smooth=1):
+        super().__init__()
+        self.bce = nn.BCEWithLogitsLoss()
+        self.dice_weight = dice_weight
+        self.smooth = smooth
+        
+    def __call__(self, outputs, targets):
+        if outputs.size() != targets.size():
+            raise ValueError("size mismatch, {} != {}".format(outputs.size(), targets.size()))
+            
+        loss = self.bce(outputs, targets)
+
+        targets = (targets == 1.0).float()
+        targets = targets.view(-1)
+        outputs = F.sigmoid(outputs)
+        outputs = outputs.view(-1)
+
+        intersection = (outputs * targets).sum()
+        dice = 2.0 * (intersection + self.smooth)  / (targets.sum() + outputs.sum() + self.smooth)
+        
+        loss -= self.dice_weight * torch.log(dice) # try with 1- dice
+
+        return loss
+
+class BCEJaccardWithLogitsLoss(nn.Module):
+    def __init__(self, jaccard_weight=1, smooth=1):
+        super().__init__()
+        self.bce = nn.BCEWithLogitsLoss()
+        self.jaccard_weight = jaccard_weight
+        self.smooth = smooth
+
+    def forward(self, outputs, targets):
+        if outputs.size() != targets.size():
+            raise ValueError("size mismatch, {} != {}".format(outputs.size(), targets.size()))
+            
+        loss = self.bce(outputs, targets)
+
+        if self.jaccard_weight:
+            targets = (targets == 1.0).float()
+            targets = targets.view(-1)
+            outputs = torch.sigmoid(outputs)
+            outputs = outputs.view(-1)
+           
+            intersection = (targets * outputs).sum()
+            union = outputs.sum() + targets.sum() - intersection
+
+            loss -= self.jaccard_weight * torch.log((intersection + self.smooth ) / (union + self.smooth )) # try with 1-dice
+        return loss
+
+
+def compute_metrics(pred, true, batch_size=16, threshold=0.5):
+    pred = pred.view(batch_size, -1)
+    true = true.view(batch_size, -1)
+    
+    pred = (pred > threshold).float()
+    true = (true > threshold).float()
+    
+    pred_sum = pred.sum(-1)
+    true_sum = true.sum(-1)
+    
+    neg_index = torch.nonzero(true_sum == 0)
+    pos_index = torch.nonzero(true_sum >= 1)
+    
+    dice_neg = (pred_sum == 0).float()
+    dice_pos = 2 * ((pred * true).sum(-1)) / ((pred + true).sum(-1))
+    
+    dice_neg = dice_neg[neg_index]
+    dice_pos = dice_pos[pos_index]
+    
+    dice = torch.cat([dice_pos, dice_neg])
+    jaccard = dice / (2 - dice)
+    
+    return dice, jaccard
+    
+class metrics:
+    def __init__(self, batch_size=16, threshold=0.5):
+        self.threshold = threshold
+        self.batchsize = batch_size
+        self.dice = []
+        self.jaccard = []
+    def collect(self, pred, true):
+        pred = torch.sigmoid(pred)
+        dice, jaccard = compute_metrics(pred, true, batch_size=self.batchsize, threshold=self.threshold)
+        self.dice.extend(dice)
+        self.jaccard.extend(jaccard)
+    def get(self):
+        dice = np.nanmean(self.dice)
+        jaccard = np.nanmean(self.jaccard)
+        return dice, jaccard
 
 
 if __name__ == "__main__":
@@ -132,20 +248,43 @@ if __name__ == "__main__":
 
     m = (0.485, 0.456, 0.406)
     s = (0.229, 0.224, 0.225)
-    transform = transforms.Compose([
+    img_transform = transforms.Compose([
         transforms.Resize((512)),
         transforms.ToTensor(),
         transforms.Normalize(mean=m, std=s)
     ])
 
-    train_dataset = AirbusDataset(ship_dir, train_df, transform=transform)
-    valid_dataset = AirbusDataset(ship_dir, valid_df, transform=None)
+    mask_transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(512),
+        transforms.ToTensor()])
+
+    val_transform = transforms.Compose([
+        transforms.Resize((512)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=m, std=s)
+    ])
+
+
+    train_dataset = AirbusDataset(ship_dir, train_df,\
+                                img_transform=img_transform, mask_transform=mask_transform)
+
+
+    valid_dataset = AirbusDataset(ship_dir, valid_df,\
+                                img_transform=val_transform, mask_transform=mask_transform)
 
     train_loader = DataLoader(dataset=train_dataset,
-                        batch_size=1,
+                        batch_size=16,
                         shuffle=True,
                         num_workers=1)
-    model = Resnet34Unet(3, 3)
+    
+    tvalid_loader = DataLoader(dataset=valid_dataset,
+                        batch_size=4,
+                        shuffle=True,
+                        num_workers=1)
+
+    
+    model = Resnet34Unet(3, 1)
     # Frozen parameters of the encoder layers
     child_counter = 0    
     for child in model.children():
@@ -156,17 +295,51 @@ if __name__ == "__main__":
             break
 
     model.to(device)
+    diceloss = BCEDiceWithLogitsLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-6, amsgrad=False)
 
+
+    model.train()
+
+    LOSS = 'BCEJaccardWithLogitsLoss'
+
+    if LOSS == 'BCEWithDigits':
+        criterion = nn.BCEWithLogitsLoss()
+    elif LOSS == 'DiceLoss':
+        criterion = DiceLoss()
+    elif LOSS == 'BCEDiceWithLogitsLoss':
+        criterion = BCEDiceWithLogitsLoss()
+    elif LOSS == 'BCEJaccardWithLogitsLoss':
+        criterion = BCEJaccardWithLogitsLoss()
+    else:
+        raise NameError("loss not supported")
+    
+
+    min_loss = np.inf
     for epoch in range(1):
-        running_loss = 0.0
+        losses = 0
+        # valid_metrics = metrics(batch_size=4)  # for validation
+
+
         for i, (imgO, labels) in enumerate(train_loader, 0):
-            print(imgO.shape)
-            print(labels.shape)
-            imgO = imgO.to(device)
-            labels = labels.to(device)
-            out = model(imgO)
-            print(out.shape)
-            assert(0)
+            optim.zero_grad()
+            imgO = imgO.to(device)       # [B, 3, H, W]
+            labels = labels.to(device)   # [B, H, W] with class indices (0, 1)
+            out = model(imgO)            # [B, 3, H, W]
+            
+            loss = criterion(out, labels)
+
+            loss.backward()
+            optim.step()
+
+            losses += loss.item()
+            print(loss)
+        
+
+        print('[%d] loss: %.3f' %  (epoch + 1, losses))
+        if losses < min_loss:
+            torch.save(model.state_dict(), "./uresnet34_best.pt")
+            min_loss = running_loss
 
 
 
