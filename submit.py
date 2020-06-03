b39097c36d308bf156bfb6c41c569079f7d7c62e
Brian Guo <kuobrian614@gmail.com>
2020-06-03 14:36:51 +0800

    update submit file

--- submit.py
+++ submit.py
@@ -11,9 +11,63 @@ import torch.nn.functional as F
 import torch.nn as nn
 from model_UResnet34 import Resnet34Unet
 from imgaug import augmenters as iaa
-from train import AirbusDataset
 from skimage.morphology import binary_opening, disk, label
+from imgaug import augmenters as iaa
 from utils import *
+from train import AirbusDataset
+
+
+def vis_validate_data(device):
+    ship_dir = "./data"
+    train_path = os.path.join(ship_dir, "train_v2")
+    test_path = os.path.join(ship_dir, "test_v2")
+
+    if not os.path.exists("./processing_data/unique.csv"):
+        unique_img_ids, masks = unique_images(ship_dir, train_path, test_path)
+        unique_img_ids.to_csv("./processing_data/unique.csv")
+    else:
+        unique_img_ids = pd.read_csv("./processing_data/unique.csv")
+        masks = pd.read_csv(os.path.join(ship_dir, 'train_ship_segmentations_v2.csv'))
+
+    # balanced training data
+    # train_df, valid_df = balanced_images(masks, unique_img_ids)
+
+    # drop images without ships
+    train_df, valid_df = drop_img_without_ships(masks, unique_img_ids)
+
+    tfs = transforms.Compose([ iaa.Sequential([
+                                        iaa.size.Resize({"height": 256, "width": 256})
+                                        ]).augment_image
+                            ])
+
+    valid_dataset = AirbusDataset(ship_dir, valid_df, transform = tfs)
+    # assert(0)
+    val_loader = DataLoader(dataset=valid_dataset,
+                            batch_size=4,
+                            shuffle=False,
+                            num_workers=4)
+    
+    model = Resnet34Unet(3, 1).to(device)
+    model.load_state_dict(torch.load("./weights/uresnet34_best.pt"))
+    model.eval()
+
+    for batch_idx, (imgO, labels) in enumerate(val_loader):
+        imgO = imgO.to(device)
+        output = model(imgO)
+        output = ((output > 0).float()) * 255
+        imgO = imgO.data.cpu()
+        output = output.data.cpu()
+
+        grid_imgs = torchvision.utils.make_grid(imgO, nrow=1)
+        grid_labels = torchvision.utils.make_grid(labels, nrow=1)
+        grid_output = torchvision.utils.make_grid(output, nrow=1)
+
+        imshow_gt_out(grid_imgs, grid_labels, grid_output)
+        plt.show()
+
+
+
+
 
 
 
@@ -46,9 +100,12 @@ def imshow_gt_out(img, mask_gt, mask_out):
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    # vis_validate_data(device)
+    # assert(0)
+
     model = Resnet34Unet(3, 1).to(device)
-    if os.path.exists("./uresnet34_best.pt"):
-        model.load_state_dict(torch.load("./uresnet34_best.pt"))
+    if os.path.exists("./weights/uresnet34_best.pt"):
+        model.load_state_dict(torch.load("./weights/uresnet34_best.pt"))
     model.eval()
 
     ship_dir = "./data"
@@ -67,9 +124,8 @@ if __name__ == "__main__":
                             shuffle=True,
                             num_workers=1)
 
+
     submission = {'ImageId': [], 'EncodedPixels': []}
-
-
     for i, (imgO, image_name) in enumerate(test_loader, 0):
         imgO = imgO.to(device)
         output = model(imgO)
@@ -89,20 +145,11 @@ if __name__ == "__main__":
                 submission['ImageId'].append(fname)
                 submission['EncodedPixels'].append(None)
 
-        assert(0)
+        # assert(0)
     
     submission_df = pd.DataFrame(submission, columns=['ImageId', 'EncodedPixels'])
-    submission_df.to_csv('submission.csv', index=False)
+    submission_df.to_csv('./submission.csv', index=False)
     submission_df.sample(10)
 
-    # output = ((output > 0).float()) * 255
-    # imgO = imgO.data.cpu()
-    # output = output.data.cpu()
 
-    # grid_imgs = torchvision.utils.make_grid(imgO, nrow=1)
-    # grid_labels = torchvision.utils.make_grid(labels, nrow=1)
-    # grid_output = torchvision.utils.make_grid(output, nrow=1)
-
-    # imshow_gt_out(grid_imgs, grid_labels, grid_output)
-    # plt.show()
 
